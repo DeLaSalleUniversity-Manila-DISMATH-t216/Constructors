c01fb61aa0ac6381d7bbbfa473c873dd9cee031a
aenoncunanan <aenon_cunanan2@yahoo.com>
2016-02-17 23:05:09 +0800

    Initial commit

---
+++ src/ph/edu/dlsu/olives/Olive.java
@@ -0,0 +1,24 @@
+package ph.edu.dlsu.olives;
+
+public class Olive {
+
+    public String name = "Kalamata";
+    public String flavor = "Grassy";
+    public long color = 0x000000;
+    private int oil = 3;
+
+    public Olive(){
+        System.out.println("Constructor of " + this.name);
+    }
+
+    public Olive(int oil) {
+        this.oil = oil;
+    }
+
+    public int crush(){
+
+        System.out.println("OUCH!");
+        return oil;
+    }
+
+}
